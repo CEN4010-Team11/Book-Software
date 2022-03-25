2c657307a4d4969c3be5fb05dbd104dc983bc4bb
francisco0115 <74680347+francisco0115@users.noreply.github.com>
2022-03-25 16:13:11 -0400

    Update CartRepository.java

--- demo/src/main/java/com/example/demo/cart/CartRepository.java
+++ demo/src/main/java/com/example/demo/cart/CartRepository.java
@@ -1,1 +1,14 @@
+package com.example.demo.book;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+  
+  //List<Cart> printList();
+  
+}
