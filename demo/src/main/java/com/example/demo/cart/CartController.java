1d5b3600c5ae10826fc5cc6a4656ef5041466621
francisco0115 <74680347+francisco0115@users.noreply.github.com>
2022-03-25 16:12:45 -0400

    Update CartController.java

--- demo/src/main/java/com/example/demo/cart/CartController.java
+++ demo/src/main/java/com/example/demo/cart/CartController.java
@@ -1,1 +1,28 @@
+package com.example.demo.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/v1/Cart")
+
+public class CartController {
+    private final CartService service;
+    @Autowired
+    public CartController(CartService service){
+        this.service = service;
+    }
+    @GetMapping
+    public List<Cart> getCart(){
+        return service.getCart();
+    }
+    @GetMapping("{CartID}")
+    public Optional<Cart> getCartByID(@PathVariable Long Cartid){
+        service.getCartByID(Cartid);
+
+    }
+
+}
