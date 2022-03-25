dea8559d60e7d81364c6941f2e94adb4ba222d8d
francisco0115 <74680347+francisco0115@users.noreply.github.com>
2022-03-25 16:11:25 -0400

    Update CartService.java

--- demo/src/main/java/com/example/demo/cart/CartService.java
+++ demo/src/main/java/com/example/demo/cart/CartService.java
@@ -1,1 +1,29 @@
+package com.example.demo.book;
 
+import com.example.demo.author.Author;
+import com.example.demo.author.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class CartService {
+    private final CartRepository rep;
+    @Autowired
+    public CartService(CartRepository rep){
+        this.rep = rep;
+    }
+    public List<Cart> getCart(){
+        return rep.findAll();
+    }
+    public Optional<Cart> getCartByID(@PathVariable Long Cartid) {
+        return rep.findCartByID(Cartid);
+    }
+}
