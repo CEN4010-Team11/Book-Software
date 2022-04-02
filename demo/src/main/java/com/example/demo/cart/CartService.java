dae56c491b3e1f2aebc86787260222cbe3144a56
francisco0115 <74680347+francisco0115@users.noreply.github.com>
2022-04-02 16:19:36 -0400

    Update CartService.java

--- demo/src/main/java/com/example/demo/cart/CartService.java
+++ demo/src/main/java/com/example/demo/cart/CartService.java
@@ -6,22 +6,48 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.book.BookRepository;
+
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+
 @Service
 public class CartService {
     private final CartRepository rep;
+    private final Cart cart;
+    private final BookRepository bookrep;
+    private ArrayList<Book> list = new ArrayList<>();
+    private Book name;
+    
     @Autowired
-    public CartService(CartRepository rep){
+    public CartService(CartRepository rep, BookRepository bookrep){
         this.rep = rep;
+        this.bookrep = bookrep;
     }
     public List<Cart> getCart(){
         return rep.findAll();
     }
     public Optional<Cart> getCartByID(Long Cartid) {
         return rep.findCartByID(Cartid);
+    }
+    public void addBook(Book title){
+        
+        Optional<Book> bookOptional = bookrep.findBookByISBN(title.getISBN());
+        if (!bookOptional.isPresent())
+        {
+            throw new IllegalStateException("The book with that ISBN is not in the database.");
+        }
+        list = cart.getList;
+        for(int i = 0; i <= list.length; i++){
+            name = list[i];
+            Optional<Book> bookOptional2 = bookrep.findBookByISBN(name.getISBN());
+            if(bookOptional.equals(bookOptional2) == true){
+                throw new IllegalStateException("The book with that ISBN is already on the cart.");
+            }              
+        }       
+        cart.addBook(title);
     }
 }
