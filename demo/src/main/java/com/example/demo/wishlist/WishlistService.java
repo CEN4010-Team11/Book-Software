c82df8934252094d39457862501290c3430f2be1
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-04-07 14:05:55 -0400

    Update WishlistService.java

--- demo/src/main/java/com/example/demo/wishlist/WishlistService.java
+++ demo/src/main/java/com/example/demo/wishlist/WishlistService.java
@@ -1,5 +1,7 @@
 package com.example.demo.wishlist;
 
+import com.example.demo.book.Book;
+import com.example.demo.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
@@ -25,5 +27,41 @@ public class WishlistService {
             }
         }
         return wishlist;
+    }
+
+    // add new wishlist
+    public void addNewWishlist(Wishlist wishlist) {
+        wishlists.add(wishlist);
+    }
+
+    // delete wishlist
+    public void deleteWishlist(String name) {
+        for (Wishlist w : wishlists) {
+            if (w.getName() == name) {
+                wishlists.remove(w);
+                break;
+            }
+        }
+    }
+
+    // get wishlist by user
+    public ArrayList<Wishlist> getWishlistByUser(User user) {
+        ArrayList<Wishlist> wishlist = new ArrayList<Wishlist>();
+        for (Wishlist w : wishlists) {
+            if (w.user == user) {
+                wishlist.add(w);
+            }
+        }
+        return wishlist;
+    }
+
+    // add book to wishlist
+    public void addBookToWishlist(Book book, Wishlist wishlist) {
+        wishlist.addBook(book);
+    }
+
+    // delete book from wishlist
+    public void deleteBookFromWishlist(Book book, Wishlist wishlist) {
+        wishlist.removeBook(book);
     }
 }
