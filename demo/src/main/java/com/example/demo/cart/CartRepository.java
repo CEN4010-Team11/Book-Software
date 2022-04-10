958c3324b29f370bb03df7a5770510c0e5140eb5
francisco0115 <74680347+francisco0115@users.noreply.github.com>
2022-04-10 16:16:42 -0400

    Update CartRepository.java

--- demo/src/main/java/com/example/demo/cart/CartRepository.java
+++ demo/src/main/java/com/example/demo/cart/CartRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +13,8 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
   
   //List<Cart> printList();
     Optional<Cart> findCartByID(Long id);
-  
+
+    @Query("SELECT c FROM Cart c WHERE c.user.id = ?1")
+    List<Cart> findByUser_ID(Long userID);
+
 }
