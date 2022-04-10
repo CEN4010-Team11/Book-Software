c112a1a054f07aeefde7970bda20cdbeed1eb0d9
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-04-10 12:04:53 -0400

    Update User.java

--- demo/src/main/java/com/example/demo/user/User.java
+++ demo/src/main/java/com/example/demo/user/User.java
@@ -1,128 +1,75 @@
-package com.example.demo.user;
-import com.example.demo.cart.Cart;
-import com.example.demo.wishlist.Wishlist;
+package com.example.demo.cart;
+
+import com.example.demo.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.book.Book;
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Table(name = "BookstoreUser")
-public class User {
+@Table
+public class Cart {
+//    @OneToOne
+//    @JoinColumn(
+//            name = "user_id",
+//            referencedColumnName = "id"
+//    )
+//    @JsonIgnoreProperties({"cart", "password", "address"})
+    private User user;
+
     @Id
     @SequenceGenerator(
-            name = "user_sequence",
-            sequenceName = "user_sequence",
+            name = "cart_sequence",
+            sequenceName = "cart_sequence",
             allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "user_sequence"
+            generator = "cart_sequence"
     )
     @Column(name = "id", updatable = false)
-    private Long id;
+    private Long ID;
 
-    @Column(name = "email", updatable = false, nullable = false)
-    String email;
+    private ArrayList<Book> bookList = new ArrayList<>();
 
-    @Column(name = "password", updatable = true, nullable = false)
-    String password;
+    public Cart(){}
 
-    @Column(name = "name")
-    String name;
-
-    @Column(name = "address")
-    String address;
-
-    /*
-    user relationships with cart and wishlists, will uncomment later
-     */
-
-    @JsonIgnore 
-    @OneToMany(mappedBy = "user", orphanRemoval = true, cascade = {CascadeType.ALL})
-    private List<Wishlist> wishlists = new ArrayList<>();
-
-    @OneToOne(cascade = {CascadeType.ALL})
-    @JoinColumn(
-            name = "cart_id",
-            referencedColumnName = "id"
-    )
-    private Cart cart;
-
-
-    public User() {
+    public Cart(User user){
+        this.user = user;
     }
 
-    public User(String email, String password, String name, String address) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.address = address;
+    public void addBook(Book title){
+        bookList.add(title);
+
+    }
+    public void setUser(User user){
+        this.user = user;
     }
 
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public User getUser(){
+        return user;
     }
 
-    public String getEmail() {
-        return email;
+    public ArrayList<Book> getList(){
+        return bookList;
     }
 
-    public String setEmail() {
-        return email;
+    public void removeBook(Book name){
+        bookList.remove(name);
     }
-
-    public String getPassword() {
-        return password;
+    public void purchase(){
+        //incrementSales();
+        bookList.clear();
     }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Cart getCart() {
-        return cart;
-    }
-
-    public void createCart(){
-        Cart newCart = new Cart();
-        this.setCart(newCart);
-        newCart.setUser(this);
-    }
-
-    public void setCart(Cart cart) {
-        this.cart = cart;
-    }
-
-    //include cart and wishlists when ready
-
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", cart=" + cart +
+        return "Cart{" +
+                "user=" + user +
+                ", ID=" + ID +
+                ", bookList=" + bookList +
                 '}';
     }
 }
