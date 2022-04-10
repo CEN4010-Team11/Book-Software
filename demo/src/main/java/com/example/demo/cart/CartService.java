2d06cce85a8b65d4f0ad94ca9a2b5735b535c45a
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-04-10 11:35:13 -0400

    Update CartService.java
    fixed errors in syntax and ensured the program can run without errors

--- demo/src/main/java/com/example/demo/cart/CartService.java
+++ demo/src/main/java/com/example/demo/cart/CartService.java
@@ -1,5 +1,6 @@
 package com.example.demo.cart;
 
+import com.example.demo.book.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.example.demo.book.BookRepository;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -17,7 +19,7 @@ import java.util.Optional;
 @Service
 public class CartService {
     private final CartRepository rep;
-    private final Cart cart;
+    private final Cart cart = new Cart();
     private final BookRepository bookrep;
     private ArrayList<Book> list = new ArrayList<>();
     private Book book;
@@ -31,41 +33,43 @@ public class CartService {
     public List<Cart> getCart(){
         return rep.findAll();
     }
+
     public Optional<Cart> getCartByID(Long Cartid) {
         return rep.findCartByID(Cartid);
     }
-    public void addBook(Book title, Long Cardid){
-        
-        Optional<Book> bookOptional = bookrep.findBookByISBN(title.getISBN());
-        if (!bookOptional.isPresent())
+
+    public void addBook(Long Cardid){
+        boolean exists = bookrep.existsById(Cardid);
+        if (!exists)
         {
-            throw new IllegalStateException("The book with that ISBN is not in the database.");
+            throw new IllegalStateException("The book with that ID is not in the database.");
         }
-        list = cart.getList;
-        for(int i = 0; i <= list.length; i++){
-            book = list[i];
+        Book book = bookrep.getById(Cardid);
+        list = cart.getList();
+        for(int i = 0; i <= list.size(); i++){
+            book = list.get(i);
             Optional<Book> bookOptional2 = bookrep.findBookByISBN(book.getISBN());
-            if(bookOptional.equals(bookOptional2) == true){
+            if(book.equals(bookOptional2) == true){
                 throw new IllegalStateException("The book with that ISBN is already on the cart.");
             }              
         }       
-        cart.addBook(title);
+        cart.addBook(book);
     }
-    public void removeBook(Book name, Long Cardid){
-        Optional<Book> bookOptional = bookrep.findBookByISBN(name.getISBN());
-        if (!bookOptional.isPresent())
+    public void removeBook(Long Cardid){
+        boolean exists = bookrep.existsById(Cardid);
+        if (!exists)
         {
-            throw new IllegalStateException("The book with that ISBN is not in the database.");
+            throw new IllegalStateException("The book with that ID is not in the database.");
         }
+        Book book = bookrep.getById(Cardid);
         list = cart.getList();
-        for(int i = 0; i <= list.length; i++){
-            book = list[i];
+        for(int i = 0; i <= list.size(); i++){
+            book = list.get(i);
             Optional<Book> bookOptional2 = bookrep.findBookByISBN(book.getISBN());
-            if(bookOptional.equals(bookOptional2) == true){
-               cart.removeBook(name);
+            if(book.equals(bookOptional2.get()) == true){
+               cart.removeBook(book);
                 check = true;
             }
-                
         }
         if(check == false){
             throw new IllegalStateException("The book with that ISBN is not on the cart.");
